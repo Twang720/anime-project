5fae55f2be16d69c799d8075bac191b837ece6e5
Timothy Wang <twang@Timothys-MacBook-Air.local>
2025-07-28 14:13:15 -0700

    create react frontend

--- server/app.js
+++ server/app.js
@@ -11,12 +11,18 @@ var apiRouter = require('./routes/api');
 
 var app = express();
 
+var corsOptions = {
+  origin: 'http://localhost:3000', // Replace with your React app's origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed request headers
+};
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
